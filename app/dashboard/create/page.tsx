a5c3154181ba8f96ade5876e62cced25b2f2a176
AkhatorEnosa <aosakhogba@gmail.com>
2025-08-09 11:37:46 +0100

    authentication and authorization with middleware

---
+++ app/dashboard/create/page.tsx
@@ -0,0 +1,12 @@
+import { Card } from '@/components/ui/card'
+import React from 'react'
+
+const CreatePostRoute = () => {
+  return (
+    <Card>
+        
+    </Card>
+  )
+}
+
+export default CreatePostRoute
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import { buttonVariants } from "@/components/ui/button"
+import { Plus } from "lucide-react"
+import Link from "next/link"
 
-const DashboardRoute = () => {
+const DashboardRoute = async () => {
   return (
-    <div>DashboardRoute</div>
+    <div>
+        <div className="flex items-center justify-between mb-4">
+            <h2 className="text-xl font-medium">Your Blog Articles</h2>
+
+            <Link className={`fixed bottom-20  right-20 ${buttonVariants({ variant: "outline", size: "lg"})}`} href="/dashboard/create">
+            <Plus/>
+            Create Post
+            </Link>
+        </div>
+    </div>
   )
 }
 
